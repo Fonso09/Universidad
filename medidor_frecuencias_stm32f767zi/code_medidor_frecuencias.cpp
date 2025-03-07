98a9ec27d12708bd7eb214b8689d7c8b357657d9
Daniel Fonseca <fonsecafelipedaniel@gmail.com>
2025-03-06 20:59:03 -0500

    Medidor de frecuencias con salida del DAC

--- medidor_frecuencias_stm32f767zi/code_medidor_frecuencias.cpp
+++ medidor_frecuencias_stm32f767zi/code_medidor_frecuencias.cpp
@@ -2,26 +2,32 @@
 void RCC_SETUP();
 void GPIO_SETUP();
 void TIM_SETUP();
+float map(int value, int fromLow, int fromHigh, int toLow, int toHigh);
 
 volatile uint32_t prev_captura = 0;
 volatile uint32_t frecuencia = 0; 
+volatile uint32_t voltaje_rpm =0;
 
 int main(){
     RCC_SETUP();
     GPIO_SETUP();
     TIM_SETUP();
+    DAC->CR |= (1<<0) |(1<<16); //DAC CHANEL 1, 2 ENABLE
 
     while(1){
-
+        voltaje_rpm = map(frecuencia,0,510,0,4095);
+        DAC->DHR12R2 = voltaje_rpm;// PA5 para salida de DAC
     }
 }
 
 void RCC_SETUP(){
     RCC->AHB1ENR |= 0xFF; //Activa los puertos de la A a la F
-    RCC->APB1ENR |= (1<<0); //Habilita TIM2, el que se usara para poner en modo captura y medir la frecuencia
+    RCC->APB1ENR |= (1<<0)|(1<<29); //Habilita TIM2, el que se usara para poner en modo captura y medir la frecuencia Habilita DAC
+    
 }
 void GPIO_SETUP(){
     GPIOA->MODER |= (2<<0); // se coloca en modo Alternante el PA0 el cual sera el encargado de leer para el TIM2
+    GPIOA->MODER |= 0xC00; //DAC PA5 canal 2 
     GPIOA->AFR[0] |= (1<<0); // AF1 para PA0 para TIM2 Channel 1
     GPIOB->MODER |= (1<<14);// LED de prueba PB7
 }
@@ -38,6 +44,11 @@ void TIM_SETUP(){
 
     NVIC_EnableIRQ(TIM2_IRQn);
     
+}
+float map(int value, int fromLow, int fromHigh, int toLow, int toHigh) {
+    if(value < fromLow) return toLow;
+    if(value > fromHigh) return toHigh;
+    return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
 }
 
 
