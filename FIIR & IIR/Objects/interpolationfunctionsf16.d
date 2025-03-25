20389657de0ecde633cdfb51638219fc3bae336c
Daniel Fonseca <fonsecafelipedaniel@gmail.com>
2025-03-24 22:39:59 -0500

    template FIR e IIR

---
+++ FIIR & IIR/Objects/interpolationfunctionsf16.d
@@ -0,0 +1,19 @@
+./objects/interpolationfunctionsf16.o: \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\InterpolationFunctions\InterpolationFunctionsF16.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\InterpolationFunctions\arm_bilinear_interp_f16.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\dsp\interpolation_functions_f16.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\arm_math_types_f16.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\arm_math_types.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\float.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\limits.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\arm_math_memory.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\dsp\none.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\arm_math_types.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\dsp\utils.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\InterpolationFunctions\arm_linear_interp_f16.c
---
+++ FIIR & IIR/Objects/svmfunctions.d
@@ -0,0 +1,25 @@
+./objects/svmfunctions.o: \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\SVMFunctions.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_linear_init_f32.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\dsp\svm_functions.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\arm_math_types.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\float.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\limits.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\arm_math_memory.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\arm_math_types.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\dsp\none.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\dsp\utils.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Include\dsp\svm_defines.h \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_linear_predict_f32.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_polynomial_init_f32.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_polynomial_predict_f32.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_rbf_init_f32.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_rbf_predict_f32.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_sigmoid_init_f32.c \
+  C:\Users\Usuario\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\SVMFunctions\arm_svm_sigmoid_predict_f32.c
