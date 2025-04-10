3757a8d2ba0e8923752892f54a503e62fa3221bb
Fonso09 <120606978+Fonso09@users.noreply.github.com>
2025-04-10 00:35:39 -0500

    Update lector_guardado.py
    mayor explicación para conectarse a la base de datos

--- taller_MQTT/lector_guardado.py
+++ taller_MQTT/lector_guardado.py
@@ -7,13 +7,13 @@ conn = mysql.connector.connect(
     host="localhost",    #se coloca esto porque tenemos abierto el puerto del contenedor de la base de datos
     user="fonso",  #se coloca el user de la base de datos
     password="fonso1",
-    database="mqtt_data" 
+    database="mqtt_data" #aquí se coloca el nombre de la base de datos (la que se coloca en el .yml del contenedor basicamente)
 )
 cursor = conn.cursor()
 
 #guardar el mensaje que se capturó del mqtt broker
 def save_to_db(topic, payload):
-    cursor.execute("INSERT INTO mqtt_msg (topic, payload) VALUES (%s, %s)",(topic, payload))
+    cursor.execute("INSERT INTO mqtt_msg (topic, payload) VALUES (%s, %s)",(topic, payload)) #aquí se coloca el nombre de la tabla que queremos modificar de la base de datos (la que se crea en el init.sql pues)
     conn.commit()
 #esto es para que cuando le llegue un mensajes por mqtt guarde en la db
 def on_msg(client, userdata, msg):
